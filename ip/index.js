a18e6287a7950b8800f4091154d1849f75def185
yunusemredurmus <128366167+yunusemredurmus@users.noreply.github.com>
2023-07-25 14:29:50 +0300

    taamlandı

--- ip/index.js
+++ ip/index.js
@@ -1,5 +1,5 @@
 //axios import buraya gelecek
-
+import axios from 'axios';
 var benimIP;
 
 
@@ -7,25 +7,25 @@ var benimIP;
 // licensed to Ergineer 2022
 require("babel-core/register");
 require("babel-polyfill");
-async function ipAdresimiAl(){
+async function ipAdresimiAl() {
 	await axios({
 		method: 'get',
 		url: 'https://apis.ergineer.com/ipadresim',
 	})
-	.then(function (response) {
-		return response.data
-	})
-	.then(function (a) {
-		benimIP=a
-	});
-}				
+		.then(function (response) {
+			return response.data
+		})
+		.then(function (a) {
+			benimIP = a
+		});
+}
 // ------------ değiştirmeyin --------------
 
 
 /*
 	ADIM 1: axios kullanarak, aşağıdaki URL'ye GET sorgusu atacağız
-    (tag içindeki yere kendi ipnizi yazarak URL'yi oluşturun):
-    https://apis.ergineer.com/ipgeoapi/<ipniz>
+	(tag içindeki yere kendi ipnizi yazarak URL'yi oluşturun):
+	https://apis.ergineer.com/ipgeoapi/<ipniz>
 	
 	NOT: Bilgisayarın IP adresini öğrenmek için: https://apis.ergineer.com/ipadresim 
 	ADIM 5'e gelene kadar fonksiyonunuzu test etmek için ip nizi URL'ye manuel olarak ekleyebilirsiniz.
@@ -38,7 +38,7 @@ async function ipAdresimiAl(){
 */
 /*
 	ADIM 3: Argümanı sadece 1 nesne kabül eden bir fonksiyon oluşturun.
-    DOM metotlarını ve özelliklerini kullanarak, şunları gerçekleştirin:
+	DOM metotlarını ve özelliklerini kullanarak, şunları gerçekleştirin:
 	
 	<div class="card">
 	<img src={ülke bayrağı url} />
@@ -51,22 +51,51 @@ async function ipAdresimiAl(){
 		<p>Para birimi: {para birimi}</p>
 		<p>ISP: {isp}</p>
 	</div>
-    </div>
+	</div>
+*/
+/*
+	ADIM 4: API'den alınan verileri kullanarak ADIM 3'te verilen yapıda bir kart oluşturun ve
+	bu kartı DOM olarak .cards elementinin içine ekleyin.
 */
 
 /*
-	ADIM 4: API'den alınan verileri kullanarak ADIM 3'te verilen yapıda bir kart oluşturun ve 
-	bu kartı DOM olarak .cards elementinin içine ekleyin. 
-*/
-
-/*
-	ADIM 5: Manuel olarak eklediğiniz IP adresini dinamiğe dönüştürün. 
-	Sayfanın en üstünde ---değiştirmeyin--- etiketleri arasında yer alan asenkron ipAdresimiAl() fonksiyonuna 
-	sorgu atarak bilgisayarınız IP adresini dinamik olarak aldıracaksınız. Bu fonksiyon asenkron olarak çağırıldığında `benimIP` değişkenine 
-	bilgisayarınızın IP adresini atayacaktır. 
-	Örnek dinamik URL kullanımı: var url = "https://apis.ergineer.com/ipgeoapi/"+benimIP; 
+	ADIM 5: Manuel olarak eklediğiniz IP adresini dinamiğe dönüştürün.
+	Sayfanın en üstünde ---değiştirmeyin--- etiketleri arasında yer alan asenkron ipAdresimiAl() fonksiyonuna
+	sorgu atarak bilgisayarınız IP adresini dinamik olarak aldıracaksınız. Bu fonksiyon asenkron olarak çağırıldığında `benimIP` değişkenine
+	bilgisayarınızın IP adresini atayacaktır.
+	Örnek dinamik URL kullanımı: var url = "https://apis.ergineer.com/ipgeoapi/"+benimIP;
 */
 
 
 
 //kodlar buraya gelecek
+
+axios.get('https://apis.ergineer.com/ipgeoapi/88.240.156.181')
+	.then(response => {
+		console.log("başarılı")
+	})
+	.catch(err => {
+		console.log("başarısız")
+	})
+
+function app(nesne) {
+	const div = document.createElement("div");
+	div.classList.add("card");
+	const img = document.createElement("img");
+	imgCard.setAttribute("src", dataFromApi["ülkebayrağı"]);
+	const div2 = document.createElement("div");
+	div2.classList.add("card-info");
+
+	const h3 = document.createElement("h3");
+	h3.classList.add("ip");
+	h3.textContent = "{ip adresi}"
+	div2.append(h3)
+	const p = document.createElement("p");
+	p.classList.add("ulke");
+	p.textContent = "ülke bilgisi $(ülke kodu)} "
+	div2.append(p)
+	const p1 = document
+
+
+
+}
